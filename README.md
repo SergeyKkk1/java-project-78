7fd90b3a6cafb37345e213fc33e3ba3d7c8bd9ac
sergey <sergey.kobelew@yandex.ru>
2026-02-09 21:21:22 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SergeyKkk1/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/SergeyKkk1/java-project-78/actions)
+## Data Validator Project
+A library which helps you validate any data in your application.
+## How to use this project.
+```
+git@github.com:SergeyKkk1/java-project-78.git
+make build
+```
+After creation of `jar` file, you can add it into project which 
+requires data validation and use it there.
---
+++ app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    application
+    checkstyle
+    id("org.sonarqube") version "7.1.0.6387"
+    id ("com.github.ben-manes.versions") version "0.53.0"
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SergeyKkk1_java-project-78")
+        property("sonar.organization", "sergeykkk1")
+    }
+}
